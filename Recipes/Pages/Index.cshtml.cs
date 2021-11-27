775357fc087ae274c9cf121f05c6ab1de29a50bf
Joe Gannon <joegannon15@gmail.com>
2021-11-27 03:13:59 -0500

    dynamic data

--- Recipes/Pages/Index.cshtml.cs
+++ Recipes/Pages/Index.cshtml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Recipes.Data;
 
 namespace Recipes.Pages
@@ -21,14 +18,69 @@ namespace Recipes.Pages
 
         public void OnGet()
         {
-            var recipes = _context.Recipe
-                .Include(x => x.Ingredients)
-                .ThenInclude(x => x.Ingredient)
-                .Include(x => x.Instructions)
-                .Where(x => x.Id == 1)
-                .ToList();
-
-
         }
+
+        public IActionResult OnGetSearch(string filter)
+        {
+            var recipes = new List<RecipeViewModel>();
+
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                recipes = _context.Recipe
+                                    .Include(x => x.Ingredients)
+                                    .ThenInclude(x => x.Ingredient)
+                                    .Include(x => x.Instructions)
+                                    .Select(x => new RecipeViewModel
+                                    {
+                                        Id = x.Id,
+                                        Title = x.Title,
+                                        Description = x.Description,
+                                        Ingredients = x.Ingredients.Select(z => new IngredientViewModel { Id = z.IngredientId, Name = z.Ingredient.Name }),
+                                        Instructions = x.Instructions.Select(z => new InstructionViewModel { Id = z.Id, Description = z.Description })
+                                    })
+                                    .ToList();
+            }
+            else
+            {
+                recipes = _context.Recipe
+                                   .Include(x => x.Ingredients)
+                                   .ThenInclude(x => x.Ingredient)
+                                   .Include(x => x.Instructions)
+                                   .Where(x => x.Description.Contains(filter) || x.Title.Contains(filter))
+                                   .Select(x => new RecipeViewModel
+                                   {
+                                       Id = x.Id,
+                                       Title = x.Title,
+                                       Description = x.Description,
+                                       Ingredients = x.Ingredients.Select(z => new IngredientViewModel { Id = z.IngredientId, Name = z.Ingredient.Name }),
+                                       Instructions = x.Instructions.Select(z => new InstructionViewModel { Id = z.Id, Description = z.Description })
+                                   })                                   
+                                   .ToList();
+            }
+
+
+            return new JsonResult(new { Recipes = recipes });
+        }
+    }
+
+    public class RecipeViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<IngredientViewModel> Ingredients { get; set; }
+        public IEnumerable<InstructionViewModel> Instructions { get; set; }
+    }
+
+    public class IngredientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class InstructionViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
     }
 }
